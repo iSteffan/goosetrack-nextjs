abf6fc64097986489be9d440ef0c9a8e4e55eee5
Stepan Skichko <st.skichko@gmail.com>
2025-02-03 12:25:36 +0200

    add some config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+
+  images: {
+    deviceSizes: [360, 480, 768, 1024, 1280, 1366, 1440, 1920, 2048],
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,18 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
+    screens: {
+      sm: '375px',
+      md: '768px',
+      xl: '1440px'
+    },
+
     extend: {
       colors: {
         background: "var(--background)",
