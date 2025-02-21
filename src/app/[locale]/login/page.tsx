e404d795cb21bd4d437c5fb27bdd54b9d92f3310
Stepan Skichko <st.skichko@gmail.com>
2025-02-21 12:36:33 +0200

    login & signup page Done

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,10 +1,35 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 import { AuthForm } from '@/components/AuthForm/AuthForm';
 
+import data from '@/data/common.json';
+
 export default function Page() {
+  const { signUp } = data;
+
   return (
-    <section className="bg-blueLight py-[155px]">
+    <section className="min-h-screen bg-blueLight py-[155px]">
       <div className="container">
         <AuthForm type="logIn" />
+
+        <div className="mt-[18px] text-center md:mt-[24px]">
+          <Link
+            href="/en/register"
+            className="scale inline-block text-[12px] font-600 leading-[1.16] text-blueMain underline md:text-[18px] md:leading-[1.33]"
+          >
+            {signUp}
+          </Link>
+        </div>
+
+        <div className="absolute right-[60px] top-[230px] hidden xl:block">
+          <Image
+            src="/image/logInGoose.png"
+            alt="goose"
+            width={368}
+            height={521}
+          />
+        </div>
       </div>
     </section>
   );
--- src/app/[locale]/register/page.tsx
+++ src/app/[locale]/register/page.tsx
@@ -1,10 +1,35 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 import { AuthForm } from '@/components/AuthForm/AuthForm';
 
+import data from '@/data/common.json';
+
 export default function Page() {
+  const { logIn } = data;
+
   return (
-    <section className="bg-blueLight py-[155px]">
+    <section className="min-h-screen bg-blueLight py-[155px]">
       <div className="container">
         <AuthForm type="signUp" />
+
+        <div className="mt-[18px] text-center md:mt-[24px]">
+          <Link
+            href="/en/login"
+            className="scale inline-block text-[12px] font-600 leading-[1.16] text-blueMain underline md:text-[18px] md:leading-[1.33]"
+          >
+            {logIn}
+          </Link>
+        </div>
+
+        <div className="absolute bottom-0 left-[49px] hidden xl:block">
+          <Image
+            src="/image/signUpGoose.png"
+            alt="goose"
+            width={400}
+            height={416}
+          />
+        </div>
       </div>
     </section>
   );
