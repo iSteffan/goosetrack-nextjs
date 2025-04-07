1036db297407f84a83c43832a12aab2672dcb7ef
Stepan Skichko <st.skichko@gmail.com>
2025-04-07 11:37:52 +0300

    Update TaskColumnCard.tsx

--- src/components/ui/TasksColumnsList/TasksColumn/ColumnsTasksList/TaskColumnCard/TaskColumnCard.tsx
+++ src/components/ui/TasksColumnsList/TasksColumn/ColumnsTasksList/TaskColumnCard/TaskColumnCard.tsx
@@ -5,6 +5,7 @@ import {
   // useMutation,
   useQueryClient,
 } from '@tanstack/react-query';
+import classNames from 'classnames';
 
 import { IUser } from '@/components/common/UserForm/UserForm';
 import { Task } from '../../../TasksColumnsList';
@@ -40,29 +41,44 @@ export const TaskColumnCard = ({ task }: TaskColumnCardProps) => {
   //   return null;
   // }
 
+  const priorityStyles = classNames(
+    'inline-block h-[20px] rounded-[4px] px-[12px] py-[4px] text-[10px] font-600 leading-[1.2] text-white',
+    {
+      'bg-radioLow': task?.priority === 'Low',
+      'bg-radioMed': task?.priority === 'Medium',
+      'bg-radioHigh': task?.priority === 'High',
+    },
+  );
+
   if (!task) {
     return null;
   }
 
   return (
     <div className="rounded-[8px] border-[1px] border-taskCardBorder px-[14px] pb-[18px] pt-[14px] dark:border-darkThemeBorder dark:bg-blackLightBg">
-      <p className="mb-[28px] block text-[14px] leading-[1.28] text-blackCustom dark:text-white">
+      <p className="mb-[28px] block overflow-hidden text-ellipsis whitespace-nowrap text-[14px] leading-[1.28] text-blackCustom dark:text-white">
         {task?.title}
       </p>
-      <div>
-        <div className="flex h-[32px] w-[32px] items-center justify-center rounded-[32px] border-[1.8px] border-blueMain text-[14px] font-700 leading-[1.28] text-blackCustom dark:text-white md:h-[44px] md:w-[44px] md:rounded-[44px] md:text-[18px]">
-          {data?.user?.avatarURL ? (
-            <Image
-              src={data?.user?.avatarURL}
-              alt="user avatar"
-              width={32}
-              height={32}
-              className="h-full w-full rounded-full object-cover"
-            />
-          ) : (
-            <p>{firstLetter}</p>
-          )}
+
+      <div className="flex justify-between">
+        <div className="flex items-end gap-[8px]">
+          <div className="flex h-[32px] w-[32px] items-center justify-center rounded-[32px] border-[1.8px] border-blueMain text-[14px] font-700 leading-[1.28] text-blackCustom dark:text-white md:h-[44px] md:w-[44px] md:rounded-[44px] md:text-[18px]">
+            {data?.user?.avatarURL ? (
+              <Image
+                src={data?.user?.avatarURL}
+                alt="user avatar"
+                width={32}
+                height={32}
+                className="h-full w-full rounded-full object-cover"
+              />
+            ) : (
+              <p>{firstLetter}</p>
+            )}
+          </div>
+
+          <p className={priorityStyles}>{task?.priority}</p>
         </div>
+
         <TaskToolbar />
       </div>
     </div>
