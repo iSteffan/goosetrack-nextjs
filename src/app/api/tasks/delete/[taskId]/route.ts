9986017b3af0fdb979d624cee758c4785a7ffc2f
Stepan Skichko <st.skichko@gmail.com>
2025-04-09 13:45:54 +0300

    update delete task route due to vercel deploy issue

--- src/app/api/tasks/delete/[taskId]/route.ts
+++ src/app/api/tasks/delete/[taskId]/route.ts
@@ -1,12 +1,66 @@
+// import { NextRequest, NextResponse } from 'next/server';
+// import { dbConnect } from '@/utils/dbConnect';
+// import Task from '@/models/Task';
+// import { authMiddleware } from '@/middleware/auth';
+
+// export async function DELETE(
+//   req: NextRequest,
+//   { params }: { params: { taskId: string } },
+// ) {
+//   const user = authMiddleware(req);
+
+//   if (!user || typeof user === 'string') {
+//     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+//   }
+
+//   try {
+//     await dbConnect();
+
+//     const { taskId } = params;
+
+//     if (!taskId) {
+//       return NextResponse.json(
+//         { message: 'Task ID is required' },
+//         { status: 400 },
+//       );
+//     }
+
+//     const task = await Task.findById(taskId);
+
+//     if (!task || task.userId !== user.id) {
+//       return NextResponse.json(
+//         { message: 'Task not found or unauthorized' },
+//         { status: 404 },
+//       );
+//     }
+
+//     await Task.findByIdAndDelete(taskId);
+
+//     return NextResponse.json(
+//       { message: 'Task deleted successfully' },
+//       { status: 200 },
+//     );
+//   } catch (error) {
+//     console.error('Failed to delete task:', error);
+//     return NextResponse.json(
+//       { message: 'Failed to delete task' },
+//       { status: 500 },
+//     );
+//   }
+// }
+
 import { NextRequest, NextResponse } from 'next/server';
 import { dbConnect } from '@/utils/dbConnect';
 import Task from '@/models/Task';
 import { authMiddleware } from '@/middleware/auth';
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { taskId: string } },
-) {
+type Context = {
+  params: {
+    taskId: string;
+  };
+};
+
+export async function DELETE(req: NextRequest, context: Context) {
   const user = authMiddleware(req);
 
   if (!user || typeof user === 'string') {
@@ -16,7 +70,7 @@ export async function DELETE(
   try {
     await dbConnect();
 
-    const { taskId } = params;
+    const { taskId } = context.params;
 
     if (!taskId) {
       return NextResponse.json(
