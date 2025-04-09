fd48373d2fabb2b491fcd75f7d212e7431b8f5ec
Stepan Skichko <st.skichko@gmail.com>
2025-04-09 14:04:18 +0300

    update update task route due to vercel deploy issue

--- src/app/api/tasks/update/[taskId]/route.ts
+++ src/app/api/tasks/update/[taskId]/route.ts
@@ -44,15 +44,60 @@
 //   }
 // }
 
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
+// import { dbConnect } from '@/utils/dbConnect';
+// import { authMiddleware } from '@/middleware/auth';
+// import Task from '@/models/Task';
+
+// export async function PATCH(
+//   req: NextRequest,
+//   context: { params: { taskId: string } },
+// ) {
+//   const user = authMiddleware(req);
+
+//   if (!user || typeof user === 'string') {
+//     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+//   }
+
+//   try {
+//     await dbConnect();
+
+//     const updates = await req.json();
+//     const taskId = context.params.taskId; // отримуємо taskId з контексту
+
+//     const task = await Task.findById(taskId);
+//     if (!task || task.userId !== user.id) {
+//       return NextResponse.json(
+//         { message: 'Not found or unauthorized' },
+//         { status: 404 },
+//       );
+//     }
+
+//     Object.assign(task, updates);
+//     await task.save();
+
+//     return NextResponse.json(
+//       { message: 'Task updated successfully', task },
+//       { status: 200 },
+//     );
+//   } catch (error) {
+//     console.error('Failed to update task:', error);
+//     return NextResponse.json(
+//       { message: 'Failed to update task' },
+//       { status: 500 },
+//     );
+//   }
+// }
+
+import { NextRequest, NextResponse } from 'next/server';
 import { dbConnect } from '@/utils/dbConnect';
 import { authMiddleware } from '@/middleware/auth';
 import Task from '@/models/Task';
 
 export async function PATCH(
   req: NextRequest,
-  context: { params: { taskId: string } },
+  { params }: { params: Promise<{ taskId: string }> },
 ) {
   const user = authMiddleware(req);
 
@@ -64,7 +109,7 @@ export async function PATCH(
     await dbConnect();
 
     const updates = await req.json();
-    const taskId = context.params.taskId; // отримуємо taskId з контексту
+    const { taskId } = await params;
 
     const task = await Task.findById(taskId);
     if (!task || task.userId !== user.id) {
