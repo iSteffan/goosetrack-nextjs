cb25a3c31d77a3442df56c137afabd1fc5cc4282
Stepan Skichko <st.skichko@gmail.com>
2025-04-21 11:57:33 +0300

    Update page.tsx
    add periodPaginator

--- src/app/[locale]/(verified)/statistics/page.tsx
+++ src/app/[locale]/(verified)/statistics/page.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { useQueryClient } from '@tanstack/react-query';
+import { format } from 'date-fns';
 
 import { IUser } from '@/store/userStore';
+import { PeriodPaginator } from '@/components/ui/PeriodPaginator/PeriodPaginator';
+import { useState } from 'react';
 
 // import { toast } from 'react-toastify';
 
@@ -13,21 +16,25 @@ export default function Page() {
   // const router = useRouter();
 
   // Перевіряємо, чи є дані перед рендером
-  if (!data?.user) {
-    return <p>Loading...</p>;
-  }
+
+  const periodType = 'day';
+  const initialDate = format(new Date(), 'yyyy-MM-dd');
+
+  const [selectedDate, setSelectedDate] = useState(initialDate);
+
+  // if (!data?.user) {
+  //   return <p>Loading...</p>;
+  // }
 
   return (
-    <div>
-      <h1>User Information</h1>
-      <div>
-        <p>Name: {data.user.name}</p>
-        <p>Email: {data.user.email}</p>
-        <p>Avatar URL: {data.user.avatarURL}</p>
-        <p>Birthday: {data.user.birthday}</p>
-        <p>Phone: {data.user.phone}</p>
-        <p>Telegram: {data.user.telegram}</p>
+    <section>
+      <div className="container pt-[40px]">
+        <PeriodPaginator
+          periodType={periodType}
+          selectedDate={selectedDate}
+          onDateChange={setSelectedDate}
+        />
       </div>
-    </div>
+    </section>
   );
 }
