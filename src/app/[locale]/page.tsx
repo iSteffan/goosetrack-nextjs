6cbe503cac7006bbe3c21e38bc8e687490c7e060
Stepan Skichko <st.skichko@gmail.com>
2025-05-06 11:56:15 +0300

    add i18n to AuthSection

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,3 @@
-import { useTranslations } from 'next-intl';
-import { Link } from '@/i18n/routing';
-
 import {
   AuthSection,
   DescriptionSection,
@@ -10,29 +7,8 @@ import {
 import { ThemeToggle } from '@/components/ui/ThemeToggle/ThemeToggle';
 
 export default function HomePage() {
-  const t = useTranslations('HomePage');
-
   return (
     <>
-      <h1>{t('title')}</h1>
-      <Link href="/account">{t('account')}</Link>
-      <div className="flex flex-col">
-        <Link
-          href="/"
-          locale="en"
-          className="bg-slate-500 text-[20px] dark:bg-orange-900"
-        >
-          Switch to English
-        </Link>
-        <Link
-          href="/"
-          locale="uk"
-          className="bg-slate-500 text-[20px] dark:bg-orange-900"
-        >
-          Switch to Ukrainian
-        </Link>
-      </div>
-
       <ThemeToggle />
 
       <AuthSection />
--- src/sections/MainPageSection/AuthSection/AuthSection.tsx
+++ src/sections/MainPageSection/AuthSection/AuthSection.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
+import { useTranslations, useLocale } from 'next-intl';
 
 import LogInIcon from '@/public/icon/logIn.svg';
 import GooseIcon from '@/public/icon/gooseAuth.svg';
-
-import data from '@/data/common.json';
+import LocaleSwitcher from '@/components/ui/LocaleSwitcher/LocaleSwitcher';
 
 export const AuthSection = () => {
-  const { logIn, signUp } = data;
+  const t = useTranslations('AuthSection');
+  const locale = useLocale();
 
   return (
     <section className="bg-blueMain py-[256px]">
@@ -17,19 +18,24 @@ export const AuthSection = () => {
           <span className="italic">Goose</span>Track
         </h1>
 
-        <div className="flex flex-col items-center gap-[16px] md:flex-row md:justify-center md:gap-[24px]">
-          <Link
-            href="/en/login"
-            className="scale flex w-[131px] justify-center gap-[6px] rounded-[16px] bg-white py-[14px] text-[14px] font-600 leading-[1.28] tracking-[-0.028px] text-blueMain"
-          >
-            {logIn} <LogInIcon className="h-[18px] w-[18px]" />
-          </Link>
-          <Link
-            href="/en/register"
-            className="scale text-[14px] font-600 leading-[1.28] text-white underline"
-          >
-            {signUp}
-          </Link>
+        <div className="flex flex-col items-center gap-[16px] md:gap-[24px]">
+          <div className="flex flex-col items-center gap-[16px] md:flex-row md:justify-center md:gap-[24px]">
+            <Link
+              href={`/${locale}/login`}
+              className="scale flex w-[131px] justify-center gap-[6px] rounded-[16px] bg-white py-[14px] text-[14px] font-600 leading-[1.28] tracking-[-0.028px] text-blueMain"
+            >
+              {t('login')} <LogInIcon className="h-[18px] w-[18px]" />
+            </Link>
+
+            <Link
+              href={`/${locale}/register`}
+              className="scale text-[14px] font-600 leading-[1.28] text-white underline"
+            >
+              {t('signup')}
+            </Link>
+          </div>
+
+          <LocaleSwitcher />
         </div>
       </div>
     </section>
