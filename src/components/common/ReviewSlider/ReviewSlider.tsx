962e24549c3928a56245b2ec1d2f3fcb62ebc51d
Stepan Skichko <st.skichko@gmail.com>
2025-05-13 13:11:52 +0300

    add basic reviewSlider

--- src/components/common/ReviewSlider/ReviewSlider.tsx
+++ src/components/common/ReviewSlider/ReviewSlider.tsx
@@ -1,10 +1,37 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+
+import { ReviewSliderCard } from '@/components/ui/ReviewSliderCard/ReviewSliderCard';
 import { IReview } from '@/sections/MainPageSection/ReviewsSection/ReviewsSection';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 interface ReviewSliderProps {
   reviews: IReview[];
 }
 
 export const ReviewSlider = ({ reviews }: ReviewSliderProps) => {
+  const swiperParams = {
+    centeredSlides: false,
+    modules: [Navigation],
+    slidesPerView: 1,
+    spaceBetween: 16,
+    breakpoints: {
+      1440: { slidesPerView: 2, spaceBetween: 24 },
+    },
+  };
+
   console.log('reviews', reviews);
-  return <div>ReviewSlider</div>;
+  return (
+    <div>
+      <Swiper {...swiperParams}>
+        {reviews.map(review => (
+          <SwiperSlide key={review._id}>
+            <ReviewSliderCard data={review} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
---
+++ src/components/ui/ReviewSliderCard/ReviewSliderCard.tsx
@@ -0,0 +1,39 @@
+import ReactStars from 'react-stars';
+
+import { IReview } from '@/sections/MainPageSection/ReviewsSection/ReviewsSection';
+import { Avatar } from '../Avatar/Avatar';
+
+interface ReviewSliderCardProps {
+  data: IReview;
+}
+export const ReviewSliderCard = ({ data }: ReviewSliderCardProps) => {
+  return (
+    <div className="border-[rgba(17, 17, 17, 0.10)] rounded-[8px] border-[1px] p-[24px]">
+      <div className="mb-[24px] flex gap-[18px]">
+        <div className="h-[50px] w-[50px]">
+          <Avatar avatarURL={data.avatarURL} name={data.username} size={50} />
+        </div>
+
+        <div>
+          <p className="fonr-700 text-[18px] text-blackCustom">
+            {data.username}
+          </p>
+
+          <ReactStars
+            count={5}
+            size={24}
+            color2={'#FFAC33'}
+            color1={'#CEC9C1'}
+            value={data.rating}
+            half={true}
+            edit={false}
+          />
+        </div>
+      </div>
+
+      <p className="h-[72px] overflow-hidden text-ellipsis text-[14px] leading-[1.28] text-blackCustom opacity-[0.7]">
+        {data.comment}
+      </p>
+    </div>
+  );
+};
